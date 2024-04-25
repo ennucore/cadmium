f3561e4ee89832fcd183db9b567b499f02914c14
Lev <lc@lev.la>
2024-04-25 12:15:41 +0200

    The code execution is working

--- agent/executor.py
+++ agent/executor.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 
-import traceback
-import io
-import sys
+import subprocess
+import tempfile
 from typing import Any
 
 
@@ -44,17 +43,13 @@ result = spiral_sweep.stl
 
 class CadqueryExecutor:
     def execute(self, script: str) -> (str, Any, bool):
-        output = io.StringIO()
-        sys.stdout = output
-        sys.stderr = output
-        result = None
-        print('script:', script)
+        with tempfile.NamedTemporaryFile(suffix='.py', mode='w', delete=False) as f:
+            postfix = '\nprint("<RESULT>" + str(result) + "</RESULT>")\n'
+            f.write(preamble + script + postfix)
+            script_path = f.name
         try:
-            exec(preamble + script)
-            return output.getvalue(), result, True
-        except Exception as e:
-            return output.getvalue() + traceback.format_exc(), result, False
-        finally:
-            sys.stdout = sys.__stdout__
-            sys.stderr = sys.__stderr__
-            output.close()
+            output = subprocess.check_output(['python', script_path], stderr=subprocess.STDOUT)
+            result = output.decode().split('<RESULT>')[1].split('</RESULT>')[0] if '<RESULT>' in output.decode() else None
+            return output.decode(), result, True
+        except subprocess.CalledProcessError as e:
+            return e.output.decode(), None, False
