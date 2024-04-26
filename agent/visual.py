5f5341a736b5620c966e90a863fac68f371af4b7
Lev <lc@lev.la>
2024-04-26 07:35:04 +0200

    visual selection

--- agent/visual.py
+++ agent/visual.py
@@ -3,6 +3,9 @@ import os
 import openai
 from cadmium.stl_to_pics.render import render
 from dotenv import load_dotenv
+from multiprocessing.pool import ThreadPool
+from itertools import chain
+import json
 import glob
 import base64
 
@@ -17,7 +20,7 @@ client = openai.OpenAI(
 
 def stl_to_paths(stl_path: str) -> list[str]:
     # get the directory of the stl file
-    stl_dir = os.path.dirname(stl_path)
+    stl_dir = os.path.dirname(stl_path) or '.'
     stl_name = os.path.basename(stl_path).split('.')[0]
     render(
         [stl_path],
@@ -56,7 +59,8 @@ def visual_feedback(stl_path: str, prompt: str) -> (str, bool):
     messages = [
         {"role": "system", "content": "Your goal is to evaluate 3d models against a prompt."},
         {"role": "user", "content": [
-            {"type": "text", "text": f"The prompt for the model is as follows: \n```\n{prompt}\n```\nPlease, look at the images and provide feedback on the model."},
+            {"type": "text", "text": f"The prompt for the model is as follows: \n```\n{prompt}\n```\nPlease, look at the images and provide feedback on the model: whether it fits the requirements and whether the user will be satisfied. "
+             "The images below are views of the 3d model from different sides: top, bottom, front, left."},
             *map(path_to_openai_image, paths)
             ]},
     ]
@@ -64,3 +68,43 @@ def visual_feedback(stl_path: str, prompt: str) -> (str, bool):
         model="gpt-4-turbo-2024-04-09",
         messages=messages,
     ).choices[0].message.content, True
+
+
+def pick_visually(stl_paths: list[str], prompt: str, n_to_pick: int = 2) -> list[str]:
+    # get images in threads
+    paths = list(map(stl_to_paths, stl_paths))
+    messages = [
+        {"role": "system", "content": f"Your goal is to select **{n_to_pick}** best 3d models based on a prompt."},
+        {"role": "user", "content": [
+            {"type": "text", "text": f"The prompt for the model is as follows: \n```\n{prompt}\n```\nPlease, look at the images and select {n_to_pick} best 3d models using the result function. "
+             "The images below are views of the 3d models from different sides: top, bottom, front, left."},
+            *chain(*map(lambda inp: [{"type": "text", "text": f"Model #{inp[0] + 1}.\nHere it is from top, bottom, front, left:"}, *map(path_to_openai_image, inp[1])], enumerate(paths)))
+            ]},
+    ]
+    
+    response = client.chat.completions.create(
+        model="openai/gpt-4-turbo-2024-04-09",
+        messages=messages,
+        tools=[{"type": "function", "function": {
+            "name": "result", "description": "Report the selected 3d models",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "selected_models": {
+                        "type": "array", "description": "The selected models, in decreasing order of preference",
+                        "items": {"type": "object", "properties": {"model_index": {"type": "number", "example": 1},
+                                                                   }}, "minItems": n_to_pick, "maxItems": n_to_pick},
+                }
+            }
+        }}],
+        tool_choice={"type": "function", "function": {"name": "result"}},
+        temperature=0.25
+    )
+
+    message = response.choices[0].message
+    data = json.loads(message.tool_calls[0].function.arguments if message.tool_calls else message.content)
+    data = data.get('result', data)
+    data = data.get('parameters', data)
+    selected_models = data.get('selected_models', [])
+    selected_models = [stl_paths[res['model_index'] - 1] for res in selected_models]
+    return selected_models
--- scripts/get_feedback.py
+++ scripts/get_feedback.py
@@ -1,8 +1,12 @@
 import sys
-from cadmium.agent.visual import visual_feedback
+from cadmium.agent.visual import visual_feedback, pick_visually
 
 
 if __name__ == '__main__':
-    stl_path = sys.argv[1]
-    prompt = sys.argv[2]
-    print(visual_feedback(stl_path, prompt))
+    stl_paths = sys.argv[2:]
+    prompt = sys.argv[1]
+    if len(stl_paths) == 1:
+        print(visual_feedback(stl_paths[0], prompt)[0])
+        sys.exit(0)
+    else:
+        print(pick_visually(stl_paths, prompt))
